d659106aef7be7ed52a0d304aa4bd848453ebf94
Kuki <kukiguptajnv@gmail.com>
2022-04-14 23:11:35 +0530

    Linked list

--- DataStructure/LinkedList/PalindromeList.java
+++ DataStructure/LinkedList/PalindromeList.java
@@ -4,12 +4,64 @@ import java.util.Scanner;
 
 public class PalindromeList {
 	
+
+	public static int lengthLL(Node<Integer> head) {
+		Node<Integer> temp=head;
+		int count=0;
+		while(temp != null) {
+			count++;
+			temp=temp.next;
+		}
+		return count;
+	}
 	
+	public static Node<Integer> reverseList(Node<Integer>head){
+		Node<Integer> tail=head;
+		Node<Integer> prev=null;
+		Node<Integer> fwd=null;
+		
+		while(tail != null) {
+			fwd=tail.next;
+			tail.next=prev;
+			prev=tail;
+			tail=fwd;
+		}
+		
+		return prev;
+	}
+	
+	public static boolean compareLinkedList(Node<Integer> node ,Node<Integer>newNode) {
+		
+		
+		
+		while( newNode != null) {
+			if(node.data == newNode.data){
+				node=node.next;
+				newNode=newNode.next;
+			}
+			else
+				return false;
+		}
+		
+		return true;
+	}
 
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
 		NodeCreation ll=new NodeCreation();
 		Node<Integer> head=ll.createLinkedListUsingNoOfNode();
+		
+		int length=lengthLL(head);
+		int index=0;
+		Node<Integer> head2=head;
+		while(index<=length/2) {
+			head2=head2.next;
+			index++;
+		}
+		Node<Integer> newNode=reverseList(head2);
+		 boolean check=compareLinkedList(head,newNode);
+		 System.out.println(check);
+		
 
 	}
 
